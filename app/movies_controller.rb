4bb2d41b918eef26dc63f3a37ee02e36d2e1258b
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-30 01:10:29 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -50,8 +50,6 @@ def can_find_by_multiple_attributes
 end
 
 def can_find_using_where_clause_and_be_sorted
-  # For this test return all movies released after 2002 and ordered by
-  # release date descending
   Movie.where("release_date > 2002").order(release_date: :desc)
 end
 
