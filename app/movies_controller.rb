28ece5f71924abaab6d0312c34fe28837307fc9b
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-30 00:51:26 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -19,7 +19,8 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = Movie.new(attributes).save
+  movie = Movie.new(attributes)
+  movie
 end
 
 def can_be_created_in_a_block(args = __)
