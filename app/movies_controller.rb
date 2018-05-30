140a0a626c36f107958f5098b789b7abab1716ff
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-30 01:09:24 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -46,9 +46,6 @@ def can_find_the_first_item_from_the_database_using_id
 end
 
 def can_find_by_multiple_attributes
-  # title == "Title"
-  # release_date == 2000
-  # director == "Me"
   Movie.find_by(title: "Title", release_date: "2000", director: "Me")
 end
 
