ef59b208bcb721cfbea73bec5898d0ee8390b39d
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-30 01:05:27 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,7 +30,7 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
 end
 
 def can_get_the_first_item_in_the_database
-  sql <<-sql
+  sql = <<-sql
     SELECT * FROM movies[0]
   sql
 end
