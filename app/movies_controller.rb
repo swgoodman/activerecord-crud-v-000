8e293702dd66ef3c439bcc27f9af02ab5fb1594c
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-30 01:01:42 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,7 +22,7 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = nil)
+def can_be_created_in_a_block(args = {title: "Wargames", release_date: 1983})
   Movie.create do |m|
     __
   end
